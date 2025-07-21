0905100d15e6a960c3c872a0966cb5c39cb46ed6
Ross Douglas <44041853+rdougla3@users.noreply.github.com>
2025-07-20 20:20:34 -0500

    Update MFA_Mail_Start.sh
    changed  to OAUTH version

--- MFA_Mail_Start.sh
+++ MFA_Mail_Start.sh
@@ -1,35 +1,35 @@
 #
-#	this script is calld by .bashrc to monitor MFA_Mail execution and 
+#	this script is calld by .bashrc to monitor MFA_Mail_OAUTH execution and 
 #	restart if needed.
 #
 #   OZINDFW 29 March 2025
 #
 
-if pgrep -f "MFA_Mail.py" >/dev/null 
+if pgrep -f "MFA_Mail_OAUTH.py" >/dev/null 
 then
-	echo "MFA_Mail Already Running"
+	echo "MFA_Mail_OAUTH Already Running"
 	exit
 else
 	source mfa_env/bin/activate
-	python MFA_Mail.py & 
-	logger "MFA_Mail starting" 
+	python MFA_Mail_OAUTH.py & 
+	logger "MFA_Mail_OAUTH starting" 
 	echo
-	echo "MFA_Mail starting"
+	echo "MFA_Mail_OAUTH starting"
 	date
 	echo
 fi
 while [ true ] 
 do
-	if pgrep -f "MFA_Mail.py" >/dev/null 
+	if pgrep -f "MFA_Mail_OAUTH.py" >/dev/null 
 	then 
 		sleep 5
 	else 
 		echo
-		echo "MFA_Mail not running, restarting " 
+		echo "MFA_Mail_OAUTH not running, restarting " 
 		date 
 		echo
-		logger "MFA_Mail not running, restarting " 
+		logger "MFA_Mail_OAUTH not running, restarting " 
 		source mfa_env/bin/activate
-		python MFA_Mail.py &
+		python MFA_Mail_OAUTH.py &
 	fi
 done;
